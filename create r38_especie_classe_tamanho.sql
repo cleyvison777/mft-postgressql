6748e4f7854602643b60690b7aa804e7bc15b2f8
Cleyvison Matos <cleyvisonmatos777@gmail.com>
2019-12-30 18:35:49 -0300

    CadAmf - Script BD p23_arvore

---
+++ create r38_especie_classe_tamanho.sql
@@ -0,0 +1,10 @@
+create table r38_especie_classe_tamanho(
+r38_cdclassetamanho bigserial not null primary key,
+r38_cdempresa bigserial,
+r38_cdlistaesp bigserial,
+r38_cdespecie bigserial,
+r38_cdequacaovolume bigserial,
+r38_crescimentoanualminimomm Integer,
+r38_crescimentoanualmaximomm Integer);
+
+
---
+++ create table d10_classe_tamanho_individuo.sql
@@ -0,0 +1,6 @@
+create table d10_classe_tamanho_individuo ( 
+d10_cdclassetamanho bigserial not null primary key,
+d10_nmclassetamanho varchar(50)
+);
+
+
