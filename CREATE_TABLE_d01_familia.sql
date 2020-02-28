2122bbba5efd1fa9ada7125ed28b90334e518d28
Cleyvison Matos <cleyvisonmatos777@gmail.com>
2020-02-28 19:38:05 -0300

    script familia e genero

--- CREATE_TABLE_d01_familia.sql
+++ CREATE_TABLE_d01_familia.sql
@@ -1,5 +1,6 @@
 CREATE TABLE d01_familia (
 d01_cdfamilia BIGSERIAL NOT NULL PRIMARY KEY,
-d01_nmfamilia VARCHAR(50),
-FOREIGN KEY (d01_cdfamilia) REFERENCES d13_empresa(d13_cdempresa));
+d01_nmfamilia VARCHAR(50));
 
+
+insert into d01_familia (d01_nmfamilia) values('teste');
--- create taable d02_genero.sql
+++ create taable d02_genero.sql
@@ -9,3 +9,5 @@ ADD CONSTRAINT FK_d02_genero_d01_familia
 FOREIGN KEY (d02_cdfamilia)
 REFERENCES d01_familia(d01_cdfamilia)
 ON DELETE CASCADE;
+
+INSERT INTO d02_genero (d02_nmgenero, d02_cdfamilia) values ('TESTE', 1)
